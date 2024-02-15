f150ffc887d6ec00a122be74e998bf9085ac8f12
Michael Freeman <mfreeman451@gmail.com>
2024-02-14 19:55:15 -0600

    🔧 cleanup

--- python/llamma_test_01.py
+++ python/llamma_test_01.py
@@ -1,12 +1,12 @@
 from flask import Flask, request, jsonify
 from llama_cpp import Llama
-import os
 
 app = Flask(__name__)
 
 # Initialize your Llama model
-model_path = "/Users/mfreeman/Downloads/mistral-7b-instruct-v0.1.Q6_K.gguf"  # Adjust path as necessary
-llm = Llama(model_path=model_path, n_ctx=8192, n_batch=512, n_threads=7, n_gpu_layers=2, verbose=True, seed=42)
+model_path = "/Users/mfreeman/Downloads/mistral-7b-instruct-v0.1.Q6_K.gguf"
+llm = Llama(model_path=model_path, n_ctx=8192, n_batch=512, n_threads=7, 
+            n_gpu_layers=2, verbose=True, seed=42)
 
 @app.route('/generate', methods=['POST'])
 def generate():
@@ -25,9 +25,11 @@ def generate():
         generated_text = output['choices'][0]['text'].replace(message, '')
 
         # Respond with generated text and usage info
-        return jsonify({"generated_text": generated_text, "usage_info": usage_info})
+        return jsonify({"generated_text": generated_text, "usage_info": 
+                        usage_info})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
