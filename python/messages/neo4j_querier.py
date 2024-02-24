138bb359d6478c68a1f60247b68845d9503b80c1
Michael Freeman <mfreeman451@gmail.com>
2024-02-24 13:24:58 -0600

    🔧 k8s nightmare

---
+++ python/messages/neo4j_querier.py
@@ -0,0 +1,17 @@
+from langchain_community.graphs import Neo4jGraph
+import os
+
+graph = Neo4jGraph(
+    url="bolt://localhost:7687",
+    username="neo4j",
+    password=os.environ.get("NEO4J_PASSWORD")
+)
+
+result = graph.query("""
+MATCH (user:User)-[:SENT]->(msg:Message)
+RETURN user.name AS userName, max(msg.timestamp) AS lastMessageTimestamp
+ORDER BY lastMessageTimestamp DESC
+LIMIT 25
+""")
+
+print(result)
