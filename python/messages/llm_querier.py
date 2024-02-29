88f59c6f7b3bc31a0e32a5458cbf963b5bc4cbd4
Michael Freeman <mfreeman451@gmail.com>
2024-02-28 19:41:43 -0600

    🔧 adding retrieverQA

--- python/messages/llm_querier.py
+++ python/messages/llm_querier.py
@@ -1,4 +1,6 @@
 from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain.chains import RetrievalQA
+from langchain.chat_models import ChatOllama
 from langchain_community.vectorstores.neo4j_vector import Neo4jVector
 from configs.settings import NEO4J_URI, NEO4J_USERNAME, NEO4J_PASSWORD
 import tracemalloc
@@ -6,38 +8,56 @@ import tracemalloc
 tracemalloc.start()
 
 model_name = "sentence-transformers/all-mpnet-base-v2"
-model_kwargs = {"device": "cuda"}
 
 # Your Sentence Transformer Configuration
-embedding_model = HuggingFaceEmbeddings(model_name=model_name, model_kwargs=model_kwargs)
+embedding_model = HuggingFaceEmbeddings(
+    model_name=model_name,
+)
+
+chat_llm = ChatOllama(
+    base_url="http://192.168.1.80:11434",
+    model="llama2:chat"
+)
 
 try:
     threadr_chat_vector = Neo4jVector.from_existing_index(
-        embedding_model, 
+        embedding_model,
         url=NEO4J_URI,
         username=NEO4J_USERNAME,
         password=NEO4J_PASSWORD,
-        index_name="message-embeddings", 
+        index_name="message-embeddings",
         embedding_node_property="embedding",
-        text_node_property="content"  # Use 'content' based on your schema
+        text_node_property="content"
     )
 except Exception as e:
     print("Error connecting to vectorStore:", e)
 
-# Your search query
+chat_retriever = RetrievalQA.from_llm(
+    llm=chat_llm,
+    retriever=threadr_chat_vector.as_retriever(),
+)
+
+
+result = chat_retriever.invoke(
+    {"query": "leku", "top_k": 5}
+)
+
+"""
 try:
     result = threadr_chat_vector.similarity_search(
         "leku",
         top_k=5
     )
-    threadr_chat_vector.close()
+    threadr_chat_vector._driver.close()
 except Exception as e:
     print("Error searching for similar documents:", e)
 
 # Display results
 for doc in result:
     try:
-        print(doc.page_content)
+        print(f"[{doc.metadata['timestamp']}] {doc.page_content}")
+        # print(doc.metadata)
     except Exception as e:
         print(f"Error displaying document: {e}")
         print(doc)
+"""
