8623f89adad8e2a69ad008c45d25615b481f9d07
Michael Freeman <mfreeman451@gmail.com>
2024-03-12 21:51:59 -0500

    ♻️ cleanup unused code

--- python/query/modules/neo4j/vector.py
+++ python/query/modules/neo4j/vector.py
@@ -40,26 +40,3 @@ def perform_similarity_search(neo4j_vector, query):
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
     return results
-
-
-def store_data_in_neo4j(documents, credentials):
-    """
-    Store and index text with Neo4j.
-    """
-    # Neo4j Aura credentials
-    url = credentials["url"]
-    username = credentials["username"]
-    password = credentials["password"]
-
-    # OpenAI credentials
-    openai_api_secret_key = credentials["openai_api_secret_key"]
-
-    # Instantiate Neo4j vector from documents
-    Neo4jVector.from_documents(
-        documents,
-        #OpenAIEmbeddings(openai_api_key=openai_api_secret_key), url=url, username=username, password=password,)
-        SentenceTransformerEmbedding(),
-        url=url,
-        username=username,
-        password=password,
-    )
