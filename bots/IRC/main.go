e4843216920a54a6245228a5ab52aa6e522d2f5f
Michael Freeman <mfreeman451@gmail.com>
2024-04-10 22:13:20 -0500

    🔧 sync

--- bots/IRC/main.go
+++ bots/IRC/main.go
@@ -3,97 +3,70 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"github.com/carverauto/threadr/bots/pkg/adapters/broker"
-	irc "github.com/carverauto/threadr/bots/pkg/adapters/messages"
+	d "github.com/carverauto/threadr/bots/pkg/adapters/messages"
 	"github.com/carverauto/threadr/bots/pkg/common"
-	pm "github.com/carverauto/threadr/bots/pkg/ports"
 	"github.com/nats-io/nats.go"
 	"log"
-	"time"
+	"os"
+	"os/signal"
 )
 
 func main() {
 	natsURL := "nats://nats.nats.svc.cluster.local:4222"
-	sendSubject := "irc"
+	sendSubject := "discord"
 	stream := "messages"
 	cmdsSubject := "incoming"
 	cmdsStream := "commands"
 	resultsSubject := "outgoing"
 	resultsStream := "results"
 
-	// create a context
 	ctx := context.Background()
 
-	// Create a CloudEvents handler, which will be used to send messages
 	cloudEventsHandler, err := broker.NewCloudEventsNATSHandler(natsURL, sendSubject, stream, false)
 	if err != nil {
 		log.Fatalf("Failed to create CloudEvents handler: %s", err)
 	}
 
-	// Create a CloudEvents handler, which will be used to publish requests (commands)
 	commandsHandler, err := broker.NewCloudEventsNATSHandler(natsURL, cmdsSubject, cmdsStream, false)
 	if err != nil {
 		log.Fatalf("Failed to create CloudEvents handler: %s", err)
 	}
 
-	// Create a Results handler, which will be used to subscribe to results
 	resultsHandler, err := broker.NewCloudEventsNATSHandler(natsURL, resultsSubject, resultsStream, true)
 	if err != nil {
 		log.Fatalf("Failed to create CloudEvents handler: %s", err)
 	}
 
-	var ircAdapter pm.MessageAdapter = irc.NewIRCAdapter()
-	if err := ircAdapter.Connect(ctx, commandsHandler); err != nil {
-		log.Fatal("Failed to connect to IRC:", err)
+	discordAdapter := d.NewDiscordAdapter() // Setup the Discord adapter
+	if err := discordAdapter.Connect(ctx, commandsHandler); err != nil {
+		log.Fatalf("Failed to connect to Discord: %v", err)
 	}
 
+	// Subscribe to NATS for handling results
 	go func() {
-		log.Println("main.go - Subscribing to results")
+		log.Println("Subscribing to results")
 		resultsHandler.Listen(resultsSubject, "results-durable", func(msg *nats.Msg) {
-			log.Printf("main.go - Received result: %s", string(msg.Data))
+			log.Printf("Received result: %s", string(msg.Data))
 			var result common.CommandResult
 			if err := json.Unmarshal(msg.Data, &result); err != nil {
-				log.Printf("main.go - Failed to unmarshal result: %s", err)
+				log.Printf("Failed to unmarshal result: %v", err)
 				return
 			}
 
-			log.Printf("main.go - Received result: %s", result)
-			err := msg.Ack()
-			if err != nil {
-				log.Printf("main.go - Failed to ack message: %s", err)
-				return
-			}
-
-			// send the results to the IRC channel
-			ircAdapter.Send(result.Content.Channel, result.Content.Response)
+			// Send the results back to the Discord channel
+			discordAdapter.Send(result.Content.Channel, result.Content.Response)
 		})
 	}()
 
-	// start a counter for received message_processing
-	msgCounter := 0
-	ircAdapter.Listen(func(ircMsg common.IRCMessage) {
-		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-		defer cancel()
+	fmt.Println("Bot is now running. Press CTRL+C to exit.")
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	<-c
 
-		// Create a CloudEvent
-		ce := broker.Message{
-			Message:   ircMsg.Message,
-			Nick:      ircMsg.Nick,
-			Channel:   ircMsg.Channel,
-			Platform:  "IRC",
-			Timestamp: time.Now(),
-		}
-
-		log.Printf("main.go - Publishing CloudEvent for message [%d]", msgCounter)
-		err := cloudEventsHandler.PublishEvent(ctx, "irc", ce)
-		if err != nil {
-			log.Printf("Failed to send CloudEvent: %v", err)
-		} else {
-			log.Printf("Sent CloudEvent for message [%d]", msgCounter)
-		}
-		msgCounter++
-	})
-
-	// Keep the application running
-	select {}
+	// Close the Discord session on shutdown
+	if err := discordAdapter.Session.Close(); err != nil {
+		log.Printf("Error closing Discord session: %v", err)
+	}
 }
--- bots/discord/main.go
+++ bots/discord/main.go
@@ -3,13 +3,12 @@ package main
 import (
 	"context"
 	"fmt"
-	"github.com/bwmarrin/discordgo"
 	"github.com/carverauto/threadr/bots/pkg/adapters/broker"
+	d "github.com/carverauto/threadr/bots/pkg/adapters/messages"
 	pm "github.com/carverauto/threadr/bots/pkg/ports"
 	"log"
 	"os"
 	"os/signal"
-	"strings"
 )
 
 func checkNilErr(e error) {
@@ -39,53 +38,19 @@ func main() {
 		log.Fatalf("Failed to create CloudEvents handler: %s", err)
 	}
 
-	var discordAdapter pm.MessageAdapter = discord.NewDiscordAdapter()
+	var discordAdapter pm.MessageAdapter = d.NewDiscordAdapter()
 	if err := discordAdapter.Connect(ctx, commandsHandler); err != nil {
 		log.Fatal("Failed to connect to Discord:", err)
 	}
 
-	discord, err := discordgo.New("Bot " + os.Getenv("DISCORD_TOKEN"))
-	checkNilErr(err)
-
-	// add a event handler
-	discord.AddHandler(newMessage)
-
-	// open session
-	openErr := discord.Open()
-	checkNilErr(openErr)
-
-	defer func(discord *discordgo.Session) {
-		err := discord.Close()
-		checkNilErr(err)
-	}(discord) // close session, after function termination
-
-	// keep bot running untill there is NO os interruption (ctrl + C)
-	fmt.Println("Bot running....")
+	fmt.Println("Bot is now running. Press CTRL+C to exit.")
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
 	<-c
-}
 
-func newMessage(discord *discordgo.Session, message *discordgo.MessageCreate) {
-
-	/* prevent bot responding to its own message
-	this is achived by looking into the message author id
-	if message.author.id is same as bot.author.id then just return
-	*/
-	if message.Author.ID == discord.State.User.ID {
-		return
-	}
-
-	// respond to user message if it contains `!help` or `!bye`
-	switch {
-	case strings.Contains(message.Content, "!help"):
-		send, err := discord.ChannelMessageSend(message.ChannelID, "Hello World😃")
-		checkNilErr(err)
-		fmt.Println("Send: ", send)
-	case strings.Contains(message.Content, "!bye"):
-		send, err := discord.ChannelMessageSend(message.ChannelID, "Good Bye👋")
-		checkNilErr(err)
-		fmt.Println("Send: ", send)
+	// Properly close the Discord session within the adapter on shutdown
+	if err := discordAdapter.(*d.DiscordAdapter).Session.Close(); err != nil {
+		log.Printf("Error closing Discord session: %v", err)
 	}
 
 }
